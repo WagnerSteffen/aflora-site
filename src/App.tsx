f89d128ff0b4d6f9e2318fc7a968075909ee2fe8
Wagner Steffen <wagnersteffen25@gmail.com>
2024-11-14 19:02:13 -0300

    check apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import About from './pages/About'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nossosprojetos" element={<Projects />} />
-        <Route path="/sobre" element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/nossosprojetos' element={<Projects />} />
+        <Route path='/sobre' element={<About />} />
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
