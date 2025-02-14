2a416ed7a4ff88638c84772f59f80000bb5ecd83
Wagner Steffen <wagnersteffen25@gmail.com>
2025-02-14 18:45:43 -0300

    update in typo error in Carousels.tsx

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -5,7 +5,7 @@ import useEmblaCarousel, {
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { cn } from "../../lib/utils";
-import { Button } from "./Button.tsx";
+import { Button } from "./button.tsx";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
